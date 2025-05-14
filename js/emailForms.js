2ce5e3e21f688d1172077fa808e3065954eb8ca8
Dave23377777 <23751662@stu.mmu.ac.uk>
2025-05-14 22:36:09 +0100

    insert menu sign up page mint

---
+++ js/emailForms.js
@@ -0,0 +1,5 @@
+/**
+ * https://salesforce.stackexchange.com/questions/230009/regex-for-email-validation
+ *  ^[^@]+@[^@]+\.[^@]+$
+ * 
+ */
--- js/insertMenu.js
+++ js/insertMenu.js
@@ -1,7 +1,24 @@
-function includeMenu() {
-  fetch('nav-bar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('menu-placeholder').innerHTML = data;
-    });
-}
+/* Functions as an appender to the htmls to append functionJ main script but with a unique name each fetch to hack no caching (else caching causing eleemnts to dispay wrong widths if first come from a different page 
+	
+	*/
+
+	
+
+	function applyAdjustments(){
+		
+		document.addEventListener('DOMContentLoaded',function() { //define here 
+		
+			
+			const script1 = document.createElement('script');
+			script1.src = `js/functionJ.js?v=${new Date().getTime()}`; //generrate unique timestamp name as v stop caching so sharted widths readjust
+			document.body.appendChild(script1);
+			});
+
+	}
+	
+	window.addEventListener('load', 
+		    applyAdjustments  // Re-run the JavaScript adjustments when the page loads
+		);
+		
+	applyAdjustments();
+		
